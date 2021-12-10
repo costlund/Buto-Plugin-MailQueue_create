52b72dca5a1914cd4547054391a6b62f59932369
Claes Ö <>
2021-12-10 10:05:18 +0100

    1.1.0

--- PluginMailQueue_create.php
+++ PluginMailQueue_create.php
@@ -26,32 +26,74 @@ class PluginMailQueue_create{
      * tag
      */
     $this->settings->set('tag', str_replace('[date]', date('Y-m-d'), $this->settings->get('tag')));
-    /*
-     * sql/users
-     */
-    $this->settings->set('sql/users', str_replace('[tag]', $this->settings->get('tag'), $this->settings->get('sql/users')));
-    /*
-     * sum
-     */
-    $sum_data = $this->mysql->runSql($this->settings->get('sql/sum'));
-    $this->settings->set('data/sum', $sum_data['data'][0]['sum']);
-    /*
-     * users
-     */
-    $users_data = $this->mysql->runSql($this->settings->get('sql/users'));
-    $this->settings->set('data/users', $users_data['data']);
-    /*
-     * mail
-     */
-    $this->settings->set('mail/message', str_replace('[sum]', $this->settings->get('data/sum'), $this->settings->get('mail/message')));
-    $this->settings->set('mail/body', $this->settings->get('mail/declarment')."\n".$this->settings->get('mail/message'));
-    foreach($this->settings->get('data/users') as $k => $v){
-      $i = new PluginWfArray($v);
-      $this->mail->create($this->settings->get('mail/subject'), $this->settings->get('mail/body'), $i->get('email'), $send_id = null, $date_from = null ,$date_to = null, $rank = null, $i->get('id'), $tag = $this->settings->get('tag'), $mail_from = null, $from_name = null, $attachment = array());
+
+    if($this->settings->get('sql')){
+      /*
+      * sql/users
+      */
+      $this->settings->set('sql/users', str_replace('[tag]', $this->settings->get('tag'), $this->settings->get('sql/users')));
+      /*
+      * sql/sum
+      * data/sum
+      */
+      $sum_data = $this->mysql->runSql($this->settings->get('sql/sum'));
+      $this->settings->set('data/sum', $sum_data['data'][0]['sum']);
+      /*
+      * sql/users
+      * data/users
+      */
+      $users_data = $this->mysql->runSql($this->settings->get('sql/users'));
+      $this->settings->set('data/users', $users_data['data']);
+      /*
+      * mail/*
+      */
+      $this->settings->set('mail/message', str_replace('[sum]', $this->settings->get('data/sum'), $this->settings->get('mail/message')));
+      $this->settings->set('mail/body', $this->settings->get('mail/declarment')."\n".$this->settings->get('mail/message'));
+      foreach($this->settings->get('data/users') as $k => $v){
+        $i = new PluginWfArray($v);
+        $this->mail->create(
+          $this->settings->get('mail/subject'), 
+          $this->settings->get('mail/body'), 
+          $i->get('email'), 
+          null, 
+          null,
+          null, 
+          null, 
+          $i->get('id'), 
+          $this->settings->get('tag')
+          );
+      }
+      /**
+      * 
+      */
+      exit($this->settings->get('data/sum').':'.sizeof($this->settings->get('data/users')));
+    }elseif($this->settings->get('sql_full')){
+      /**
+       * sql_full
+       */
+      $this->settings->set('sql_full', str_replace('[tag]', $this->settings->get('tag'), $this->settings->get('sql_full')));
+      $rs = $this->mysql->runSql($this->settings->get('sql_full'));
+      if($rs['num_rows']){
+        foreach($rs['data'] as $k => $v){
+          $rs['data'][$k]['subject'] = $this->settings->get('mail/subject');
+          $rs['data'][$k]['body'] = str_replace('[mail_text]', $v['mail_text'], $this->settings->get('mail/message'));
+        }
+        foreach($rs['data'] as $k => $v){
+          $i = new PluginWfArray($v);
+          $this->mail->create(
+            $i->get('subject'), 
+            $i->get('body'), 
+            $i->get('email'), 
+            null, 
+            null,
+            null, 
+            null, 
+            $i->get('id'), 
+            $this->settings->get('tag')
+            );
+        }
+      }
+      exit($rs['num_rows'].' created');
     }
-    /**
-     * 
-     */
-    exit($this->settings->get('data/sum').':'.sizeof($this->settings->get('data/users')));
   }
 }
--- README.md
+++ README.md
@@ -1,23 +1,45 @@
 # Buto-Plugin-MailQueue_create
-Create mail in table mailqueue_queue.
-
-## Example
-Cron url /mail_alert/create.
+Create mail with just a few params automatic via cron job.
+Mail data is stored in table mailqueue_queue.
 
 ## Settings
-Create email for each user once per day.
+This example using url /mail_alert/create (should be used as a cron job).
 ```
 plugin_modules:
   mail_alert:
     plugin: mail/queue_create
     settings:
       mysql: 'yml:/../buto_data/theme/[theme]/mysql.yml'
-      tag: 'errors_[date]'
-      sql:
-        sum: select count(id) as sum from errorlog_log where left(created_at,10)=left(date_add(now(), interval -1 day),10) and HTTP_HOST<>'localhost'
-        users: "select id, email from account where id not in (select account_id from mailqueue_queue where tag='[tag]')"
+```
+### Example 1
+Create email for each user once per day.
+Count rows in table account and send to all users not having this mail for the day.
+This works well if all users should have the same message.
+```
       mail:
-        subject: Error log
-        declarment: Daily report on how many errors yesterday.
-        message: 'There was [sum] errors yesterday (not localhost).'
+        subject: Subject
+        message: 'There are [sum] accounts just now.'
+        declarment: Daily report on how many accounts we have.
+      tag: 'account_count_[date]'
+      sql:
+        sum: select count(id) as sum from account
+        users: "select id, email from account where id not in (select account_id from mailqueue_queue where tag='[tag]')"
+```
+### Example 2
+This example shows how to check for user content. Mandatory outputs are id, email, mail_text.
+```
+      mail:
+        subject: Login check
+        message: 'Just a friendly reminder. You have not signed in since [mail_text]. Please come back.'
+      tag: 'logincheck_[date]'
+      sql_full: |
+        select 
+        a.id,
+        a.email,
+        (select date                  from account_log where account_id=a.id order by date desc limit 1) as mail_text,
+        (select datediff(now(), date) from account_log where account_id=a.id order by date desc limit 1) as last_login_days
+        from account as a
+        where a.id not in (select account_id from mailqueue_queue where tag='[tag]')
+        and not isnull(a.email)
+        having last_login_days>7
 ```
